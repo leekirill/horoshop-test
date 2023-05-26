f428042848241394e5f3785b6f4f0891d39e7710
ваше имя <cyril.lee1997@gmail.com>
2023-05-26 15:06:56 +0200

    пофиксил ошибку валидации и отредачил scss

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -125,8 +125,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 var _default = {
-  warn: 'Введи шо-нибудь',
-  error: 'Поправь ошибки, ало',
+  warn: 'Заполните поля',
+  error: 'Поправь ошибки',
   success: 'Заявка успешно отправлена'
 };
 exports.default = _default;
